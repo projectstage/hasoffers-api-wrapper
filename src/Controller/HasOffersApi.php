405e0a5aff11e516c95ce819d5fb3ab7165ee19c
carsten <carsten@fortelmedia.com>
2017-07-06 14:23:05 +0200

    2nd initial commit

---
+++ src/Controller/HasOffersApi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace HasoffersApiWrapper;
+
+/**
+ * Created by PhpStorm.
+ * User: carsten
+ * Date: 7/6/17
+ * Time: 12:55 PM
+ */
+class HasOffersApi
+{
+
+    public function __construct()
+    {
+    }
+}
