2eb2d0ed14c5ca47b3b704105e0ad3cb5b148787
carsten <carsten@fortelmedia.com>
2017-07-06 17:30:49 +0200

    initialize the API, Exceptions, prepare connect

--- src/Controller/HasOffersApi.php
+++ src/Controller/HasOffersApi.php
@@ -1,22 +1,143 @@
 <?php
-
-namespace HasoffersApiWrapper;
-
 /**
  * Created by PhpStorm.
  * User: carsten
  * Date: 7/6/17
  * Time: 12:55 PM
  */
+
+/**
+ * Class HasOffersApi
+ * @package HasoffersApiWrapper
+ */
 class HasOffersApi
 {
+
+    /**
+     * @var string
+     */
+    protected $network_id;
+
+    /**
+     * @var string
+     */
+    protected $api_key;
+
+    /**
+     * @var string
+     */
+    protected $api_doamin;
+
+    /**
+     * @var string
+     */
+    protected $api_url_part = 'api.hasoffers.com';
+
+    /**
+     * @var string
+     */
+    protected $protocoll = 'https';
+
+    /**
+     * @var string
+     */
+    protected $target;
+
+    /**
+     * @var string
+     */
+    protected $method;
+
 
     public function __construct()
     {
     }
 
-    public function accessCall()
+    /**
+     * @param string $network_id
+     * @param string $api_key
+     * @param string $target
+     * @param string $method
+     * @param array $params
+     * @throws ApiKeyEmptyException
+     * @throws MethodEmptyException
+     * @throws NetworkIdEmptyException
+     * @throws TargetEmptyException
+     */
+    public function prepareConnect(string $network_id, string $api_key, string $target, string $method, array $params = [])
     {
+        if($network_id == '') {
+            throw new NetworkIdEmptyException('Network ID seems to be empty');
+        }
 
+        if($api_key == '') {
+            throw new ApiKeyEmptyException('Api key seems to be empty');
+        }
+
+        if($target == '') {
+            throw new TargetEmptyException('Target seems to be empty');
+        }
+
+        if($method == '') {
+            throw new MethodEmptyException('Method seems to be empty');
+        }
+
+        /*
+         * https://NETWORKID.api.hasoffers.com/Apiv3/json?NetworkToken=APIKEY&Target=Offer&Method=findAll
+
+Breaking the URL down:
+
+Base URL: https://NETWORKID.api.hasoffers.com/Apiv3/json?
+API Key: NetworkToken=APIKEY
+Controller: Target=Offer
+Method: Method=findAll
+         */
     }
+
+    /**
+     * @return string
+     */
+    public function getNetworkId(): string
+    {
+        return $this->network_id;
+    }
+
+    /**
+     * @param string $network_id
+     * @return $this
+     */
+    public function setNetworkId(string $network_id)
+    {
+        $this->network_id = $network_id;
+
+        $this->api_doamin = $this->$this->protocoll.'://'.$this->network_id.'.'.$this->api_url_part;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey(): string
+    {
+        return $this->api_key;
+    }
+
+    /**
+     * @param string $api_key
+     * @return $this
+     */
+    public function setApiKey(string $api_key)
+    {
+        $this->api_key = $api_key;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiDoamin(): string
+    {
+        return $this->api_doamin;
+    }
+
 }
---
+++ src/Exceptions/NetworkIdEmptyException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: carsten
+ * Date: 7/6/17
+ * Time: 5:12 PM
+ */
+
+/**
+ * Class NetworkIdEmptyException
+ * @package HasoffersApiWrapper\Exceptions
+ */
+class NetworkIdEmptyException extends \Exception
+{
+
+}
